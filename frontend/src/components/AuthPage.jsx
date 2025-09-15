c8426f6061b4c0a778b2b22036311c14321174c5
Andrey Abramov <abramow-a.v@ya.ru>
2025-09-15 16:37:13 +0300

    step 3 - start

--- frontend/src/components/AuthPage.jsx
+++ frontend/src/components/AuthPage.jsx
@@ -1,48 +1,58 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Formik, useFormik } from 'formik'
-import { Form, Button, Container } from 'react-bootstrap'
+import { Form, Button, Container, Card } from 'react-bootstrap'
+import axios from 'axios';
 import './AuthPage.css'
 
 const AuthPage = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const myStorage = window.localStorage
+
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
     },
     onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2))
+      console.log(JSON.stringify(values, null, 2))  // TODO rewrite to axios
     },
   })
 
   return (
     <Formik>
       <Container max-width="300px">
-        <h1>This is Auth Page</h1>
-        <Form onSubmit={formik.handleSubmit}>
+        <Card>
+          <Card.Header>Войти</Card.Header>
+          <Card.Body>
+            <Form onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3" controlId="usermane">
-            <Form.Label>Username</Form.Label>
+            {/* <Form.Label>Username</Form.Label> */}
             <Form.Control
               name="username"
               type="name"
-              placeholder="Username"
+              placeholder="Имя пользователя"
               onChange={formik.handleChange}
               value={formik.values.username}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="Password">
-            <Form.Label>Password</Form.Label>
+            {/* <Form.Label>Password</Form.Label> */}
             <Form.Control
               name="password"
               type="password"
-              placeholder="Password"
+              placeholder="Пароль"
               onChange={formik.handleChange}
               value={formik.values.password}
             />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <Button variant="primary" type="submit" className='float-end'>
+            Войти
           </Button>
         </Form>
+          </Card.Body>
+          </Card>
       </Container>
     </Formik>
   )
--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+//import routes from '../routes.js'
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'))
+
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` }
+  }
+
+  return {}
+}
 
 const MainPage = () => {
-  return (
-    <div>
-      <h1>This is Main Page</h1>
-    </div>
-  )
-}
+  const [content, setContent] = useState('')
+  useEffect(() => {
+    const fetchContent = async () => {
+      const { data } = await axios.get(routes.usersPath(), { headers: getAuthHeader() })
+      setContent(data)
+    };
+
+    fetchContent()
+  }, [])
+
+  return content && <p>{content}</p>
+};
 
 export default MainPage
